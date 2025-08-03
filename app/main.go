172fdbb4dfd61c95fd38a05bb15c038767bf1ee6
Mehriddin Nozimov <mehriddin0507@gmail.com>
2025-08-03 20:33:46 +0500

    add multiple ping/pong support

--- app/main.go
+++ app/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -12,7 +15,9 @@ var PORT = 6379
 var HOST = "0.0.0.0"
 var PROTOCOL = "tcp"
 
-var PONG_MESSAGE = []byte("+PONG\r\n")
+var PONG_RESPONSE = []byte("+PONG\r\n")
+var PING_COMMAND = []byte("PING")
+var UNKNOWN_COMMAND_RESPONSE = []byte("UNKNOWN COMMAND RECIEVED.\n")
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -31,7 +36,42 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			continue
 		} else {
-			conn.Write(PONG_MESSAGE)
+			handleClient(conn)
 		}
+
+	}
+}
+
+var DELIMEITER_NEW_LINE byte = '\n'
+
+func handleClient(conn net.Conn) error {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+
+	for {
+		line, err := reader.ReadBytes(DELIMEITER_NEW_LINE)
+		if err != nil {
+			if err != io.EOF {
+				return nil
+			} else {
+				return err
+			}
+		}
+
+		cleaned := bytes.TrimSpace(line)
+		response := handleRequest(cleaned)
+		if len(response) > 0 {
+			conn.Write(response)
+		}
+	}
+}
+
+func handleRequest(cmd []byte) []byte {
+	fmt.Printf("Request: %s\n", cmd)
+	if bytes.Equal(cmd, PING_COMMAND) {
+		return PONG_RESPONSE
+	} else {
+		return UNKNOWN_COMMAND_RESPONSE
 	}
 }
